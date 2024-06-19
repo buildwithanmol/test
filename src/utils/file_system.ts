8c0bb5364cc47178db91293092a079736e16333a
Aryan Vishwakarma <developwithanmol@gmail.com>
2024-06-19 03:10:38 -0700

    test: moved usage_folder to app dir from public_dir

--- src/utils/file_system.ts
+++ src/utils/file_system.ts
@@ -3,7 +3,7 @@ import {promises as fs } from 'fs';
 
 export const get_file_data = async (category: string, component: string) => {
     try {
-        const file = await fs.readFile(process.cwd() + `/public/usage-components/${category}/${component}-usage.tsx`, 'utf8');
+        const file = await fs.readFile(process.cwd() + `/app/usage-components/${category}/${component}-usage.tsx`, 'utf8');
         return file;
 
     } catch (error) {
