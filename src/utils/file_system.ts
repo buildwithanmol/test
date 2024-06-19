0fdde16c8ce59263e8627602faa373c6dbd20ab6
Aryan Vishwakarma <developwithanmol@gmail.com>
2024-06-19 03:42:02 -0700

    test: checking

--- src/utils/file_system.ts
+++ src/utils/file_system.ts
@@ -3,13 +3,8 @@ import {promises as fs } from 'fs';
 
 export const get_file_data = async (category: string, component: string) => {
     try {
-        const file = await fs.readFile(process.cwd() + `/public/usage-components/${category}/${component}-usage.tsx`, 'utf8');
-        
-        await fs.writeFile(process.cwd() +  `/public/usage-components/${category}/${component}-usage.txt`, file);
-        
-        const data = await fs.readFile(process.cwd() + `/public/usage-components/${category}/${component}-usage.tsx`, 'utf-8')
-
-        return data; 
+        const file = await fs.readFile(process.cwd() + `/public/usage-components/${category}/${component}-usage.txt`, 'utf8');
+        return file; 
     } catch (error) {
         console.log(error)
         return null
